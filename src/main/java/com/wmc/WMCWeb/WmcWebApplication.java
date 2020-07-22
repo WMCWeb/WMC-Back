00314ba32fe158fc733f40fd4c246917bbf835a2
Lee KH <leta6603@gmail.com>
2020-07-22 22:30:33 +0900

    Initial commit

---
+++ src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
@@ -0,0 +1,14 @@
+package com.wmc.WMCWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmcWebApplication.class, args);
+		System.out.println("hello hi nice to meet you");
+	}
+
+}
