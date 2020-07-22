fc5bd49e4febbd25712c0d15f0274f672189f1da
lg <lg@192.168.1.44>
2020-07-23 01:02:38 +0900

    test

--- src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
+++ src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
@@ -9,5 +9,7 @@ public class WmcWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WmcWebApplication.class, args);
 		System.out.println("hello hi nice to meet you");
+		System.out.println("wow");
 	}
+
 }
