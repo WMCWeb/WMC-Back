4172eef1c5fb1a2e20266853a9bcfb3e62243c1b
LKHZ <leta6603@gmail.com>
2020-07-23 20:33:38 +0900

    Initial commit

--- src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
+++ src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
@@ -10,5 +10,6 @@ public class WmcWebApplication {
 		SpringApplication.run(WmcWebApplication.class, args);
 		System.out.println("hello hi nice to meet you");
 		System.out.println("혜린");
+		System.out.println("경훈");
 	}
 }
