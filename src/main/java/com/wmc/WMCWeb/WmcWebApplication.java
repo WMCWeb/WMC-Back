6dd31b9eb4e1dab886cbc8bbb5182b00591b51ff
이경훈 <leta6603@gmail.com>
2020-08-06 22:08:34 +0900

    공지 날짜로 조회 개발
    공지 날짜로 조회하는 기능을 개발했다.
    아직 테스트는 해보지 못한 상태라서 추후 테스트가 필요하다.

--- src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
+++ src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
@@ -3,9 +3,6 @@ package com.wmc.WMCWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-
 @SpringBootApplication
 public class WmcWebApplication {
 
