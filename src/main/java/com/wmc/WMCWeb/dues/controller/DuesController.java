893d9a7aee48bd207412c03cf58a52916aea24ad
이경훈 <leta6603@gmail.com>
2020-09-21 15:31:07 +0900

    DuesController클래스 내 DuesSeervicefinal 키워드 다시 추가

--- src/main/java/com/wmc/WMCWeb/dues/controller/DuesController.java
+++ src/main/java/com/wmc/WMCWeb/dues/controller/DuesController.java
@@ -1,14 +1,10 @@
 package com.wmc.WMCWeb.dues.controller;
 
 import com.wmc.WMCWeb.dues.domain.Dues;
-import com.wmc.WMCWeb.dues.repository.MemoryDuesRepository;
 import com.wmc.WMCWeb.dues.service.DuesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +12,7 @@ import java.util.Map;
 @RequestMapping("/dues")
 public class DuesController {
 
-    private DuesService duesService;
+    private final DuesService duesService;
 
     @Autowired
     public DuesController(DuesService duesService){
