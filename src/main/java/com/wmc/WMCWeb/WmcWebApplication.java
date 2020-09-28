f030a42cf26c4877fe5970f4838a23eccf61119e
이경훈 <leta6603@gmail.com>
2020-09-28 16:59:40 +0900

    간단한 리팩토링
    쓸데없는 주석, import 제거했다.

--- src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
+++ src/main/java/com/wmc/WMCWeb/WmcWebApplication.java
@@ -1,7 +1,5 @@
 package com.wmc.WMCWeb;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -11,11 +9,6 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:/application.properties")
 public class WmcWebApplication {
 
-	// 2020.09.28 이경훈: Log4j2 logger 사용법
-	/*
-	private static final Logger logger = LogManager.getLogger(WmcWebApplication.class);
-
-	*/
 	public static void main(String[] args) {
 		SpringApplication.run(WmcWebApplication.class, args);
 		System.out.println("혜린, 경훈, 수빈");
